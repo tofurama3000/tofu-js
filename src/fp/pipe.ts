40f19f669d5aeb395aaa2a0cd30a0a6d71d63d76
matttolman <tofurama3000@users.noreply.github.com>
2018-10-27 16:31:21 -0600

    Fixed pipe type

--- src/fp/pipe.ts
+++ src/fp/pipe.ts
@@ -1,11 +1,21 @@
 import { isFunction } from '../is';
 
-export function pipe(paramOrFunc: ((p: any) => any) | any, ...functions: Array<(p: any) => any>) {
+export type PipedFunction = (p: any) => any;
+
+export interface PipeFunc {
+  (func: PipedFunction, ...functions: Array<(p: any) => any>): (param: any) => any;
+  (param: any, func: PipedFunction, ...functions: Array<(p: any) => any>): any;
+}
+
+export const pipe: PipeFunc = (
+  paramOrFunc: ((p: any) => any) | any,
+  ...functions: Array<(p: any) => any>
+) => {
   if (isFunction(paramOrFunc)) {
     return chain(paramOrFunc, ...functions);
   }
   return chain(...functions)(paramOrFunc);
-}
+};
 
 function chain(...funcs: Array<(p: any) => any>) {
   return (param: any) => {
