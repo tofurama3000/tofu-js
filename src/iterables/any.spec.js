a4ce7a35abe23f1423905c408ddeeee2d2da6993
matttolman <tofurama3000@hotmail.com>
2019-12-10 06:53:11 -0700

    Fixed formatting

--- src/iterables/any.spec.js
+++ src/iterables/any.spec.js
@@ -5,7 +5,7 @@ describe('Iterable any', () => {
     expect(any(x => x < 15, [1, 3, 5, 7, 9, 10, 11, 12])).toBe(true);
     expect(any(x => x % 2 === 0, [2, 4, 6, 8, 10, 11])).toBe(true);
     expect(any(x => x < 15, [15, 32, 51, 27, 9, 10, 11, 12])).toBe(true);
-    
+
     expect(any(x => x < 15, [15, 32, 51, 27, 90, 510, 111, 122])).toBe(false);
     expect(any(x => x % 2 === 0, [21, 45, 67, 89, 13, 11])).toBe(false);
   });
