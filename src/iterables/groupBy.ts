b746b0b53b2cf056fee053def0e92226467369f3
matttolman <tofurama3000@users.noreply.github.com>
2020-12-27 21:59:33 -0700

    added more functions

--- src/iterables/groupBy.ts
+++ src/iterables/groupBy.ts
@@ -4,7 +4,7 @@
  */
 import { curry } from '../fp/curry';
 import { toIterableOrEmpty } from './toIterableOrEmpty';
-import { get } from '../obj/get';
+import { isArray, isFunction, isNil, isObject } from '../is';
 
 /**
  * Takes an iterable of values and groups them by the result of calling a function
@@ -28,24 +28,55 @@ export const groupBy = curry(function(
   return result;
 });
 
-export const groupByKey = (
-  key: string,
-  iterable: Iterable<any>
-): { [key: string]: any[]; [key: number]: any[] } => groupBy(get(key), iterable);
+const get = curry((key: string | number, obj: any) =>
+  (isArray(obj) || isObject(obj) || isFunction(obj)) && !isNil(obj) && key in obj ? obj[key] : null
+);
 
-export const indexBy = (
-  func: (any) => any,
-  iterable: Iterable<any>
-): { [key: string]: any; [key: number]: any } => {
+/**
+ * Takes an iterable of values and groups them by a key
+ *
+ * @generator
+ * @kind function
+ * @param {string|number} key The key to group by
+ * @param {Iterable<any>} iterable An iterable to iterate over
+ * @returns {Object} An object where the result of func is the key and an array of elements that provided that key is the value
+ */
+export const groupByKey = curry((key: string | number, iterable: Iterable<any>): {
+  [key: string]: any[];
+  [key: number]: any[];
+} => groupBy(get(key), iterable));
+
+/**
+ * Takes an iterable of values and indexes them by the result of a function
+ *
+ * @generator
+ * @kind function
+ * @param {function} func The function to index by
+ * @param {Iterable<any>} iterable An iterable to iterate over
+ * @returns {Object} An object where the result of func is the key and an array of elements that provided that key is the value
+ */
+export const indexBy = curry((func: (any) => any, iterable: Iterable<any>): {
+  [key: string]: any;
+  [key: number]: any;
+} => {
   const result = {};
   for (const val of toIterableOrEmpty(iterable)) {
     const key = func(val);
     result[key] = val;
   }
   return result;
-};
+});
 
-export const indexByKey = (
-  key: string,
-  iterable: Iterable<any>
-): { [key: string]: any[]; [key: number]: any[] } => indexBy(get(key), iterable);
+/**
+ * Takes an iterable of values and indexes them by a key
+ *
+ * @generator
+ * @kind function
+ * @param {string|number} key The key to index by
+ * @param {Iterable<any>} iterable An iterable to iterate over
+ * @returns {Object} An object where the result of func is the key and an array of elements that provided that key is the value
+ */
+export const indexByKey = curry((key: string, iterable: Iterable<any>): {
+  [key: string]: any[];
+  [key: number]: any[];
+} => indexBy(get(key), iterable));
--- src/obj/keys.ts
+++ src/obj/keys.ts
@@ -6,3 +6,12 @@
 export function keys(obj: Object) {
   return (Object.keys(obj) as Array<string | Symbol>).concat(Object.getOwnPropertySymbols(obj));
 }
+
+/**
+ * Returns all values for an object (from both strings and symbol keys)
+ * @param {Object} obj
+ * @return any[]
+ */
+export function values(obj: Object) {
+  return keys(obj).map((key: any) => obj[key])
+}
